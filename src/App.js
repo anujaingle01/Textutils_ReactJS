30001c93c94befd9c6ecdf94e876d4d0da2231f8
Anuja M. Ingle <124343328+anujaingle01@users.noreply.github.com>
2023-03-28 16:35:31 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import TextForm from './Components/TextForm';
 import React, {useState} from 'react';
 import About from './Components/About';
 import Alert from './Components/Alert';
+// import * as ReactDOMClient from 'react-dom/client';
+// ReactDOMClient.createRoot(/*...*/);
+// ReactDOMClient.hydrateRoot(/*...*/);
 
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom';
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App(){
-  const[mode,setMode] = useState('light');
+  const[mode,setMode] = useState('dark');
   // Whether Dark Mode is Enabled or not.
   const[alert,setAlert] = useState(null)
 
@@ -27,48 +29,56 @@ function App(){
     },3000);
   }
   
-  const toggleMode = ()=>{
-    if(mode === 'light'){
+  const removeBodyClasses = ()=>{
+    document.body.classList.remove('bg-light');
+    document.body.classList.remove('bg-dark');
+    document.body.classList.remove('bg-success');
+    document.body.classList.remove('bg-primary');
+    document.body.classList.remove('bg-danger');
+    document.body.classList.remove('bg-warning');
+    document.body.classList.remove('text-light');
+    document.body.classList.remove('text-dark');
+  }
+  
+  const toggleMode = (cls)=>{
+    removeBodyClasses ();
+    console.log(cls)
+    document.body.classList.add('bg-'+cls)
+    if(cls === 'dark' || cls === 'primary' || cls === 'danger' || cls === 'success') {
+      document.body.classList.add('text-light');
+      setMode('light');
+    } else if(cls === 'light' || cls === 'warning') {
+      document.body.classList.add('text-dark');
       setMode('dark');
-      document.body.style.backgroundColor = "#042743";
-      showAlert(" Dark mode has been Enabled","success");
-      document.title = 'Textutils - Dark Mode';
+    }
+  }
+      // document.title = 'Textutils - Dark Mode';
       // setInterval(()=>{
       //   document.title='TextUtils is Amazing Mode';
       // },2000);
       // setInterval(()=>{
       //   document.title='Install TextUtils Now';
       // },1500);
-    }
-    else{
-      setMode('light');                      
-      document.body.style.backgroundColor = "white";
-      showAlert(" Light mode has been Enabled","success");
-      document.title = 'Textutils - Light Mode';
-
-    }
-  }
-
+   
   return(
     <> 
     {/* <Navbar/> */}
-    <Router>
-      <Navbar title = " TextUtils " mode={mode} toggleMode={toggleMode} />
-      <Alert alert ={alert}/>
-      <div className = "container my-3">
-      <Switch>
-       {/* /users --> Component 1
-        /users/home --> Component 2 */}
-
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/">
-            <TextForm showAlert={showAlert} heading = " Enter the text to Analyse Below " mode={mode}/>
-          </Route>
-      </Switch>
+    <BrowserRouter>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+      <div className ="container my-3">
+      <Routes>
+            {/* /users --> Component 1
+            /users/home --> Component 2 */}
+             {/* <Route exact path="/about">
+            <About mode={mode}/> */}
+            {/* <Route exact path="/">
+            <TextForm showAlert={showAlert} heading = " Enter the text to Analyse Below " mode={mode}/> */}
+              <Route exact path="/about" element ={<About />}/> 
+              <Route exact path="/" element ={<TextForm showAlert={showAlert} heading = "Try Textutils - Word Counter,Character Counter,Remove Extra Spaces" mode={mode}/>}/>
+      </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
     {/* <About/>  */}
     </>
   );
