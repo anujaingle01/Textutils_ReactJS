3dd18cc31378e8a394f9ec6c9c948e84da67bc56
Anuja M. Ingle <124343328+anujaingle01@users.noreply.github.com>
2023-03-28 16:33:23 +0530

    Add files via upload

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
-    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+    <div>
+    <nav className={`navbar navbar-expand-lg navbar-light bg-light`}>
     <div className="container-fluid">
     
     <Link className="navbar-brand" to="/">{props.title}</Link> 
@@ -16,11 +17,11 @@ export default function Navbar(props) {
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         
         <li className="nav-item">
-          <Link className="nav-Link active" aria-current="page" to="/">Home </Link>
+          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
        
         <li className="nav-item">
-          <Link className="nav-Link" to="/about">{props.aboutText}</Link>
+          <Link className="nav-link" to="/about">About</Link>
         </li>
         
       </ul>
@@ -28,24 +29,37 @@ export default function Navbar(props) {
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-primary" type="submit">Search</button>
         </form>*/}
+
+        <div className="d-flex">
+          <div className='bg-primary rounded mx-2' onClick={()=> {props.toggleMode ('primary')}} style={{height :'30px', width :'30px',cursor: 'pointer'}}>
+          </div>
+          <div className='bg-danger rounded mx-2' onClick={()=> {props.toggleMode ('danger')}} style={{height :'30px', width :'30px',cursor: 'pointer'}}>
+          </div>
+          <div className='bg-success rounded mx-2' onClick={()=> {props.toggleMode ('success')}} style={{height :'30px', width :'30px',cursor: 'pointer'}}>
+          </div>
+          <div className='bg-warning rounded mx-2' onClick={()=> {props.toggleMode ('warning')}} style={{height :'30px', width :'30px',cursor: 'pointer'}}>
+          </div>
+        </div>
+
         <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
-  <input className="form-check-input" onClick={props.toggleMode}
+  <input className="form-check-input" onClick={()=> {props.toggleMode (null)}}
   type="checkbox" id="flexSwitchCheckDefault"/>
-  <label className="form-check-label" htmlfor="flexSwitchCheckDefault">Enable Mode</label>
+  <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Toggle Mode</label>
 </div>
     </div>
   </div>
 </nav>
+</div>
   )
 }
 
 Navbar.propTypes = {
     title : PropTypes.string.isRequired,
-    aboutText : PropTypes.string.isRequired
+    aboutus : PropTypes.string
 }
 
-Navbar.defaultProps = {
-    title : 'Set Title',
-    aboutText : 'About'
-};
+// Navbar.defaultProps = {
+//     title : 'Set Title',
+//     aboutText : 'About'
+// };
 
